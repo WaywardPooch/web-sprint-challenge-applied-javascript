ad96787ad27ebaf956f20b6a0544341ec8f8b377
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-03 06:56:13 -0700

    created header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,42 @@
+/*
+  TASK 1
+  ---------------------
+  Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+  The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+  The text inside elements will be set using their `textContent` property (NOT `innerText`).
+  
+  <div class="header">
+    <span class="date">{ date }</span>
+    <h1>{ title }</h1>
+    <span class="temp">{ temp }</span>
+  </div>
+*/
+
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
-}
+  // Create Elements
+  const header = document.createElement("div");
+  const headerDate = document.createElement("span");
+  const headerTitle = document.createElement("h1");
+  const headerTemp = document.createElement("span");
+
+  // Append Elements
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+  // Assign Classes
+  header.classList.add("header");
+  headerDate.classList.add("date");
+  headerTemp.classList.add("temp");
+
+  // Assign Content
+  headerDate.textContent = date;
+  headerTitle.textContent = title;
+  headerTemp.textContent = temp;
+
+  // Return Markup
+  return header;
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,6 +45,6 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
