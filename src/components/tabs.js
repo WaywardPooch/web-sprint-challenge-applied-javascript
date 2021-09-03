645206553e922362079e1e08c606a84a3dd418ea
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-03 07:45:16 -0700

    created tabsAppender; completed tasks 3-4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 /*
   TASK 3
   ---------------------
@@ -31,14 +33,27 @@ const Tabs = (topics) => {
   return topicsTab;
 };
 
-const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+/*
+  TASK 4
+  ---------------------
+  Implement this function which takes a css selector as its only argument.
+  It should obtain topics from the endpoint: `http://localhost:5000/api/topics` 
+  (test it in Postman/HTTPie!). Find the array of topics inside the response, 
+  and create the tabs using the Tabs component. Append the tabs to the element 
+  in the DOM that matches the selector passed to the function.
+*/
+
+const tabsAppender = async (selector) => {
+  // Convert "selector" to a true entry point
+  const entryPoint = document.querySelector(selector);
+  try {
+    // Send a get request for "topic data" to the local API
+    const topicsData = await axios.get("http://localhost:5000/api/topics");
+    // Create topics using topicsData and append them to the DOM
+    entryPoint.appendChild(Tabs(topicsData["data"]["topics"]));
+  } catch (error) {
+    console.log(`ERROR: Could not fetch data! Problem: ${error}`);
+  }
 };
 
 export { Tabs, tabsAppender };
